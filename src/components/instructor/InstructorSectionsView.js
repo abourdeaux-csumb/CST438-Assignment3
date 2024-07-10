99090333f90737cdddd83d57ed9c64cc4a6d6a4c
Rahim Sarpas <127182093+sarp8099@users.noreply.github.com>
2024-07-09 23:34:16 -0700

    Update InstructorSectionsView.js

--- src/components/instructor/InstructorSectionsView.js
+++ src/components/instructor/InstructorSectionsView.js
@@ -1,89 +1,84 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { SERVER_URL } from '../../Constants';
 import { Link } from 'react-router-dom';
-import {SERVER_URL} from '../../Constants';
 
-const InstructorSectionsView = (props) => {
-    const headers = ['SecNo', 'CourseId', 'SecId',  'Year', 'Semester', 'Building', 'Room', 'Times', '', ''];
-    const displayHeaders = ['SecNo', 'CourseId', 'SecId', 'Building', 'Room', 'Times'];
+const InstructorSectionsView = () => {
+    const [term, setTerm] = useState({ year: '', semester: '' });
     const [sections, setSections] = useState([]);
-    const [search, setSearch] = useState({courseId:'', year:'', semester:''});
     const [message, setMessage] = useState('Manage assignments and grades.');
 
-    const handleSearchChange = (event) => {
-        setSearch({...search, [event.target.name]: event.target.value});
+    const onChange = (event) => {
+        setTerm({ ...term, [event.target.name]: event.target.value });
     }
 
-    const fetchSections = async () => {
-        if (search.year === '' || search.semester === '') {
-            setMessage("Enter search parameters");
+    const fetchSections = useCallback(async () => {
+        if (term.year === '' || term.semester === '') {
+            setMessage("");
         } else {
             try {
-                const response = await fetch(`${SERVER_URL}/sections?email=dwisneski@csumb.edu&year=${search.year}&semester=${search.semester}`,
-                {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                });
+                const response = await fetch(`${SERVER_URL}/sections?year=${term.year}&semester=${term.semester}&email=dwisneski@csumb.edu`);
+
                 if (response.ok) {
-                    console.log("No error");
                     const data = await response.json();
                     setSections(data);
                 } else {
-                    console.log("error");
                     const rc = await response.json();
                     setMessage(rc.message);
                 }
-            } catch(err) {
-                setMessage("network error: "+err);
+            } catch (err) {
+                setMessage("network error: " + err);
             }
         }
-    }
+	}, [term.year, term.semester]); // dependencies
 
 
-    useEffect(() => {
-        fetchSections();
-    }, [fetchSections]);
+	useEffect(() => {
+		fetchSections();
+	}, [fetchSections]);
 
-    return(
-        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-            <h1>Instructor Home</h1>
+    return (
+        <>
+            <table className="Center">
+                <tbody>
+                    <tr>
+                        <td>Year:</td>
+                        <td><input type="text" id="year" name="year" value={term.year} onChange={onChange} /></td>
+                    </tr>
+                    <tr>
+                        <td>Semester:</td>
+                        <td><input type="text" id="semester" name="semester" value={term.semester} onChange={onChange} /></td>
+                    </tr>
+                </tbody>
+            </table>
+            <Link to='/sections' state={term}>Show Sections</Link>
             <h4>{message}</h4>
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '250px', marginBottom: '10px'}}>
-                    <label>Year:</label>
-                    <input type="text" name="year" onChange={handleSearchChange}/>
-                </div>
-                <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '250px', marginBottom: '10px'}}>
-                    <label>Semester:</label>
-                    <input type="text" name="semester" onChange={handleSearchChange}/>
-                </div>
-                <br/>
-                <button onClick={fetchSections}>Show Sections</button>
-            </div>
-            <br/>
-            <table className="Center" >
+            <table className="Center">
                 <thead>
-                <tr>
-                    {displayHeaders.map((s, idx) => (<th key={idx}>{s}</th>))}
-                </tr>
+                    <tr>
+                        <th>SecNo</th>
+                        <th>CourseId</th>
+                        <th>SecId</th>
+                        <th>Building</th>
+                        <th>Room</th>
+                        <th>Times</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {sections.map((s) => (
+                    {sections.map((s) => (
                         <tr key={s.secNo}>
-                        <td>{s.secNo}</td>
-                        <td>{s.courseId}</td>
-                        <td>{s.secId}</td>
-                        <td>{s.building}</td>
-                        <td>{s.room}</td>
-                        <td>{s.times}</td>
-                        <td><Link to="/enrollments" state={s}>Enrollments</Link></td>
-                        <td><Link to="/assignments" state={s}>Assignments</Link></td>
+                            <td>{s.secNo}</td>
+                            <td>{s.courseId}</td>
+                            <td>{s.secId}</td>
+                            <td>{s.building}</td>
+                            <td>{s.room}</td>
+                            <td>{s.times}</td>
+                            <td><Link to="/enrollments" state={s}>Enrollments</Link></td>
+                            <td><Link to="/assignments" state={s}>Assignments</Link></td>
                         </tr>
                     ))}
                 </tbody>
             </table>
-        </div>
+        </>
     );
 }
 
