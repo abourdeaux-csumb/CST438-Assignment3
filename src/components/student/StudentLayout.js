1ead40ca4af127d47eea1f1bff10abd34a167ee3
Aaron Bourdeaux <abourdeaux@csumb.edu>
2024-08-01 20:22:07 -0400

    Reverting id deletions #2

--- src/components/student/StudentLayout.js
+++ src/components/student/StudentLayout.js
@@ -14,8 +14,8 @@ export const StudentRouter = (props) => {
               <Route path="/" element={<StudentLayout />} >
                 <Route index element={<StudentHome />} />
                 <Route path="studentAssignments" element={<AssignmentsStudentView />} />
-                <Route path="schedule" id="scheduleLink" element={<ScheduleView />} />
-                <Route path="addCourse" id="enrollLink" element={<CourseEnroll />} />
+                <Route path="schedule" element={<ScheduleView />} />
+                <Route path="addCourse" element={<CourseEnroll />} />
                 <Route path="transcript" element={<Transcript />} />
                 <Route path="logout" element={<Logout logout={props.logout}/>} />
               </Route>
@@ -45,8 +45,8 @@ export const StudentLayout = (props) => {
     <>
       <nav>
         <Link to="/">Home</Link> &nbsp;|&nbsp;   
-        <Link to="/schedule">View Class Schedule</Link>&nbsp;|&nbsp;  
-        <Link to="/addCourse">Enroll in a class</Link>&nbsp;|&nbsp;
+        <Link to="/schedule" id="scheduleLink">View Class Schedule</Link>&nbsp;|&nbsp;
+        <Link to="/addCourse" id="enrollLink">Enroll in a class</Link>&nbsp;|&nbsp;
         <Link to="/studentAssignments">View Assignments</Link>&nbsp;|&nbsp;  
         <Link to="/transcript">View Transcript</Link>&nbsp;|&nbsp; 
         <Link to="/logout">Logout</Link>
